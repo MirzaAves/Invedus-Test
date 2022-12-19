23aa954b402a05ee8103450a7cf301261351a743
MirzaAves <105174970+MirzaAves@users.noreply.github.com>
2022-12-19 17:40:22 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getStorage } from "firebase/storage";
  
 // Initialize Firebase
 const app = initializeApp ({
-    apiKey: "AIzaSyCqInA3rpAknJT8bXOEyRWkVCnpMVajM-E",
-    authDomain: "inved-test.firebaseapp.com",
-    projectId: "inved-test",
-    storageBucket: "inved-test.appspot.com",
-    messagingSenderId: "911022250220",
-    appId: "1:911022250220:web:3db80501becf3617b6adc9",
-    measurementId: "G-0L83H8CE2K"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 });
  
 // Firebase storage reference
